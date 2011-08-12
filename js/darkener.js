7e046628ea5ec52702940e1bcd16221337756990
Sam Ward <sams@live.co.uk>
2011-08-12 20:44:19 +0100

    Button functions correctly - design issues need fixing

--- js/darkener.js
+++ js/darkener.js
@@ -20,34 +20,72 @@ function getTintedColor(color, v) {
 }
 
 $('#before input').change(function() {
-	updateAfterBox();
+    updateAfterBox();
 
 });
+
+var darkenHeading = "<h1><span style='color: #11cd8a;'>Darken</span><span style='color: #CCC'>My</span>Colour</h1><h2> Enter your hex colour - get a darkened version.</h2>";
+var lightenHeading = "<h1><span style='color: #11cd8a;'>Lighten</span><span style='color: #CCC'>My</span>Colour</h1><h2> Enter your hex colour - get a lightened version.</h2>";
+var MODE = "lighten";
 
 $('#change-mode').click(function() {
+	
+	if (MODE == "lighten")
+	{
+    	$('#color-container header').html(lightenHeading);
+    	$('body').css('background', '#CCC');
+    	$('#before input, #after').css('border', '10px solid #000');
+		$('header h2').css('color', '#000');
+		
+		MODE = "darken";
+	}
+	else if (MODE == "darken")
+	{
+		$('#color-container header').html(darkenHeading);
+		$('body').css('background', '#222');
+		$('#before input, #after').css('border', '10px solid #FFF');
+		$('header h2').css('color', '#f2f2f2');
+		
+		MODE = "lighten";
+	}
+	
 
-
+	updateAfterBox(); 
 });
 
+// Tint the before box depending on the MODE
+function tintBeforeColor() {
+	if (MODE == "lighten")
+	{
+		return getTintedColor($('#before input').val(), -50);
+	}
+	else if (MODE == "darken")
+	{
+		return getTintedColor($('#before input').val(), 50);
+	}
+}
+
+// Update after box with new background color and text
 function updateAfterBox() {
 
-	$('#before input').focus();
-	// get tinted colour
-	var original = $('#before input').val();
-	var tinted = getTintedColor( $('#before input').val(), -50);
-	
-	// remove #
-	tinted = tinted.substring(1,tinted.length);
-	
-	$('#after').css('background', '#' + tinted);
-	$('#after').css('color',  '#' + original);
+    $('#before input').focus();
+    // get tinted colour
+    var original = $('#before input').val();
+    var tinted = tintBeforeColor();
+
+    // remove #
+    tinted = tinted.substring(1, tinted.length);
+
+    $('#after').css('background', '#' + tinted);
+    $('#after').css('color', '#' + original);
     $('#after h2').text(tinted);
-    
+
     $('.color').css('color', '#' + tinted);
 }
 
 $(document).ready(function() {
 
+	$('body').css('background', '#666');
     $('#before input').val('11CD8A');
-	updateAfterBox();
+    updateAfterBox();
 });
